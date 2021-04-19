2a8d750ab0832c3e65e4c9c4df4e08d9ac0291b3
Renan-Frota <81182262+Renan-Frota@users.noreply.github.com>
2021-04-19 20:26:57 -0300

    Update atividade11.py

--- atividade11.py
+++ atividade11.py
@@ -1,3 +1,3 @@
-lista = [2, 3, 4, 5, 6]
+lista = [5, 3, 4, 5, 9]
 
-print(str.join( '2 ', '3', '4', '5'map (lambda x: str(x) filter(lambda x: ..., lista)) ) )
+pprint(str.join('---', map(lambda x: str(x), filter(lambda x: x > 5 and x < 9, lista))))
